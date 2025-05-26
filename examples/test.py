7678923e1b5027038f3d3c8338b36c11fc803b8b
menisadi <menisadi@gmail.com>
2025-05-26 15:06:31 +0300

    added function in an inner indent

--- examples/test.py
+++ examples/test.py
@@ -12,6 +12,10 @@ def greet(name):
 
 
 if __name__ == "__main__":
+
+    def inner_func(x):
+        print(multiply_numbers(x, x))
+
     print(add_numbers(3, 5))
     print(multiply_numbers(4, 7))
     print(greet("Alice"))
